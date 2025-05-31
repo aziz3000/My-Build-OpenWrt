770b7243276707ec92ee9bafcaf55d46f6b7c309
Aziz <55750354+aziz3000@users.noreply.github.com>
2025-05-31 22:36:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 #echo 'src-git fichenx https://github.com/fichenx/openwrt-package' >>feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
