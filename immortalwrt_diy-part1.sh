1363a5cc511b587e804e7b0df81afae2259dcfba
Aziz <55750354+aziz3000@users.noreply.github.com>
2025-06-01 20:22:06 +0800

    Update immortalwrt_diy-part1.sh

--- immortalwrt_diy-part1.sh
+++ immortalwrt_diy-part1.sh
@@ -23,5 +23,5 @@
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 #git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 #git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
